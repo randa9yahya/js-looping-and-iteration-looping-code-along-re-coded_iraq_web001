9d5707eb5c4824cc6dff8a80701fd0356b770f6c
randa yahya <randaalrayan96@gmial.com>
2020-05-15 10:01:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+// const gift=["Ada", "Brendan", "Ali"];
+function writeCards (arrayOfNames){
+   const gift = [];
+  for(let i=0;i < arrayOfNames.length;i++){
+    gift.push(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!` );
+  }
+  return gift;
+}
+function countDown(number){
+while (number >= 0) {
+  console.log(number--);
+}
+}
