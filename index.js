5f545c7df8d6f8cff61b21c5020823c6cc2bc3a8
randa yahya <randaalrayan96@gmial.com>
2020-05-15 20:16:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 function writeCards (arrayOfNames){
    const gift = [];
   for(let i=0;i < arrayOfNames.length;i++){
+<<<<<<< HEAD
     gift.push(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!` );
   }
   return gift;
@@ -12,3 +13,18 @@ while (number >= 0) {
   console.log(number--);
 }
 }
+=======
+    gift.push(`Thank you, ${arrayOfNames[i]}, for the wonderful birthday gift!` );
+  }
+  return gift;
+}
+function countDown(){
+let i = 10;
+while (i > 0) {
+  console.log(count--);
+}
+return i;
+}
+
+writeCards(["mohhamed", "randa"])
+>>>>>>> b37c14cc1c9029a872e69970e33cf6a8c2a7d879
